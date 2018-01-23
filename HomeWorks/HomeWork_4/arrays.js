e107af5fb936f1d9c801d5c97f38793695e61c97
Denis Neporada <Denis.Neporada@netent.com>
2018-01-23 18:03:24 +0100

    Home work for Lesson_3

--- HomeWorks/HomeWork_4/arrays.js
+++ HomeWorks/HomeWork_4/arrays.js
@@ -1,7 +1,7 @@
 //Task 1
 
-function isPal (){
-    var word = prompt ("Enter any word to check is it polindrome or not: ");
+function isPal() {
+    var word = prompt("Enter any word to check is it polindrome or not: ");
     var reverseWord = word.split("");
     reverseWord.reverse();
     reverseWord.join("");
@@ -11,6 +11,7 @@ function isPal (){
         console.log("It's not polindrome")
     }
 }
+
 isPal();
 
 //Task 2
@@ -31,16 +32,18 @@ function anClean(arr) {
 anClean(arr);
 
 //Task 3
-var arr = [ 'rrrA', 'tobor', 'ekil', 'dooG', 'esoR' ];
+var arr = ['rrrA', 'tobor', 'ekil', 'dooG', 'esoR'];
 
 function splitArrrayToString() {
     var toStr = arr.join(' ').split('').reverse().join('');
     console.log(toStr);
 }
+
 splitArrrayToString();
 
 //Task 4
 sum = 0;
+
 function getSum(n) {
     for (i = 1; i <= n; i++) {
         sum += i;
@@ -53,6 +56,7 @@ getSum(100);
 //
 
 sum = 0;
+
 function getSum(n) {
     for (i = 0; i <= n; i++) {
         if (i % 2 === 0) {
@@ -66,39 +70,142 @@ getSum(100);
 
 //Task 5
 
-    //askArgument = prompt('Please put any number:  ');
 function callMeAgain() {
-    var arr = [], askArgument,
-        sum = 0;
+    var arr = [],
+        askArgument,
+        total = 0;
 
     while (arr.length < 3) {
-        askArgument = prompt('Please put any number:  ');
+        askArgument = +prompt('Please put any number:  ');
 
-        if (!isNaN(askArgument)){
+        if (!isNaN(askArgument)) {
             arr.push(askArgument);
+
         } else {
-        console.log('null');
-        return true;
+            console.log('null');
+            return true;
         }
     }
-    for (var i = 0; i < arr.length; i++) {
-        sum += parseInt(arr[i]);
-    }
-    console.log(sum);
+    for (var i = 0; i < arr.length; total += arr[i++]) ;
+    console.log(total);
 }
-callMeAgain();
-// function callMeAgain2 () {
-//
-//     if (arr.length < 3 && !isNaN(askArgument)) {
-//         arr.push(askArgument);
-//         askArgument = prompt('Please put any number:  ');
-//         for (var i = 0; i < arr.length; i++) {
-//             sum += arr[i];
-//             console.log(sum);
-//             }
-//         } else {
-//         console.log('null')
-//     }
-// }
 
+callMeAgain();
+
+// Task 6
+var arr = ['Max', 'Den', 'Vladimer'];
+
+function callMeAgain() {
+    divide = arr.sort();
+    divide = arr.join(',');
+    console.log(divide);
+}
+
+callMeAgain(arr);
+
+// Task 7
+
+var arr = [
+    {name: "L1", age: 45},
+    {name: "L2", age: 20},
+    {name: "L3", age: 10},
+    {name: "L4", age: 78},
+    {name: "L5", age: 41},
+    {name: "L6", age: 10}
+];
+
+function ageSort() {
+    age = arr.sort(function (current, next) {
+        return current.age - next.age;
+    });
+    console.log(age);
+}
+
+ageSort(arr);
+
+// Task 8
+
+var people = [];
+
+
+step();
+
+function step () {
+    var record = askNameAndAge();
+
+    if (checkExist(people, record) === false) {
+        people.push(record);
+        step();
+    }
+}
+
+function checkExist (arr , findElement) {
+    var exist = false;
+
+    for (var i = 0; i < arr.length; i++){
+        if ( findElement.name === arr[i].name && findElement.age === arr[i].age){
+            console.log("Такой пользователь уже существует");
+            exist = true;
+        }
+    }
+    return exist;
+}
+
+function askNameAndAge() {
+    var name, age;
+
+    name = prompt("Please, type your name: ");
+
+    age = prompt("Please, type your age: ");
+    age = parseInt(age);
+
+    return {
+        name: name,
+        age: age
+    };
+}
+
+// Task 9
+
+function calculateNumbersAmount () {
+    var numbers = [23, 12, 245, 34225, 2], dividedElementsArray, counter = 0;
+
+    str = numbers.join('');
+
+    dividedElementsArray = str.match(/2/g);
+
+    console.log(dividedElementsArray.length)
+}
+calculateNumbersAmount();
+
+// Task 10
+
+var arr =  ['1', '2', '3', '4', '5', '6'],
+    newArray = [];
+
+function changeArr (){
+    for ( var i = 0; i < arr.length/2; i++) {
+        newArray.push(arr[i] + arr[arr.length - (i + 1)])
+    }
+    return newArray.join('-');
+}
+changeArr();
+
+
+// Task 11
+
+var arr1 =  [1, 2, 3, 4, 5, 6],
+    arr2 =  [1, 2, 3, 4, ],
+    sum = [];
+
+function arraysSum () {
+
+    for ( var i = 0; i < arr1.lenhgt || i < arr2.length; i++) {
+       sum.push(arr1[i] + arr2[i]);
+    }
+}
+arraysSum(arr1, arr2);
+console.log(sum);
+
+// Task 12
 
