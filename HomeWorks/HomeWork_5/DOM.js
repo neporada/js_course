a036ddf28f17b888f81ff1acce831a7a51a3d1e6
Denis Neporada <Denis.Neporada@netent.com>
2018-01-31 15:42:10 +0100

    Home work for Lesson_3

---
+++ HomeWorks/HomeWork_5/DOM.js
@@ -0,0 +1,34 @@
+// Task 3
+var formSeach = document.getElementById('form');
+console.log(formSeach);
+
+// Task 4
+
+console.log(document.getElementById('form#main').children.length);
+
+// Task 5
+
+var element = document.getElementById('main');
+var parent = element.parentElement;
+console.log(parent.tagName === 'body');
+
+// Task 6
+
+document.body.style.backgroundColor = 'green';
+
+// Task 7
+
+document.body.style.backgroundColor = 'black';
+document.body.style.color = 'green';
+
+// Task 8
+
+var body = document.body;
+document.body.style.backgroundColor = 'black';
+document.body.style.color = 'white';
+
+console.log(document.body.style.backgroundColor + ', ' + document.body.style.color );
+
+console.log(getComputedStyle(body).backgroundColor + ', ' + getComputedStyle(body).color);
+
+
